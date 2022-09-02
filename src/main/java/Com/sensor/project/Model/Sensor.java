9e148135658f71b4aa4ca4c8c584d7d2a5dad8e7
Thilina <hellowthilinafive@gmail.com>
2022-09-02 18:09:25 +0530

    Initialize application with sensor model

---
+++ src/main/java/Com/sensor/project/Model/Sensor.java
@@ -0,0 +1,11 @@
+package Com.sensor.project.Model;
+
+public class Sensor {
+
+    private Long dataId;
+    private int sensorId;
+    private String dateTime;
+    private String dataValue;
+
+
+}
